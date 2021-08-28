b7dd023868704264036713f53283828b718a26fd
joshuamchavez2 <joshua.m.chavez2@gmail.com>
2021-08-27 22:26:18 -0500

    Updated Planning in Final.  Saved Plan to README.md

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-Introduce yourself and your project goals at the very beginning of your notebook walkthrough.
+
+Plan
+Step 1
+     Create a new repository, update the .gitignore, create a README.md with the data dictionary, project and business goals, and come up with initial hypotheses.
+
+Step 2
+     Explore the SQL Codeup database. Create a SQL script to select all the features and observations. Create a function in python called get_telco_data() that uses the previously defined SQL script to pull the data from the Codeup database then save the new file as acquire.py.
+
+Step 3
+     Create a function and name it clean_telco(). This function will drop any useless features, remove duplicates, double check data-types, find any null values, decide what do with null values, and encode the features using pd.get_dummies(). This clean data function will be saved on a new file called prepare.py
+
+Step 4
+     On the same prepare file, create split_telco_data(). This function will split the cleaned data into three data sets named train, validate, and test. Lastly, create prep_telco_data() that will call clean_telco() and split_telco_data(). It will return the three data sets train, validate, and test.
+
+Step 5
+     In the explore phase, I plan on running the predefined explore_univariate(), explore_bivariate(), and explore_multivariate() by importing the explore.py file. I plan on looking through the graphs and evaluate each feature to see if they play a part in churn. Exploration will also include two hypotheses, setting of alpha, statistical tests, rejecting or failing to reject the null hypothesis, and documentation of the findings and takeaways.
+
+Step 6
+     Once exploration is complete and I have defined which features I plan on using I will then create three machine learning models plus my baseline model. I will establish a baseline accuracy and document well. For my machine learning models, I will be using a DecisionTreeClassifier, RandomForestClassifier, and KNeighborsClassifier. After I MAKE the models I will FIT the models and then I will USE the models. I will use the accuracy of the train and validate data to determine if the difference is within my predefined threshold. Once I have filtered out the models that have passed, I can then pick the model with highest validate accuracy to run on my final test data.
+
+Step 7
+     Create csv file with the measurement id, the probability of the target values, and the model's prediction for each observation in my test dataset.
+
+Step 8
+     Lastly, I will document conclusions, recommendations, and takeaways in the final report notebook.
 
 Summarize your findings at the beginning like you would for an Executive Summary. Just because you don't have a slide deck for this presentation, doesn't mean you throw out everything you learned from Storytelling.
 
--- acquire.py
+++ acquire.py
@@ -4,88 +4,11 @@ import numpy as np
 import os
 from env import host, user, password
 
-###################### Acquire Titanic Data ######################
+
 
 def get_db_url(url="employees"):
     url = f'mysql+pymysql://{user}:{password}@{host}/{url}'
     return url
-    
-    
-    
-def new_titanic_data():
-    '''
-    This function reads the titanic data from the Codeup db into a df,
-    write it to a csv file, and returns the df.
-    '''
-    # Create SQL query.
-    sql_query = 'SELECT * FROM passengers'
-    
-    # Read in DataFrame from Codeup db.
-    df = pd.read_sql(sql_query, get_db_url('titanic_db'))
-    
-    return df
-
-
-
-def get_titanic_data():
-    '''
-    This function reads in titanic data from Codeup database, writes data to
-    a csv file if a local file does not exist, and returns a df.
-    '''
-    if os.path.isfile('titanic_df.csv'):
-        
-        # If csv file exists, read in data from csv file.
-        df = pd.read_csv('titanic_df.csv', index_col=0)
-        
-    else:
-        
-        # Read fresh data from db into a DataFrame.
-        df = new_titanic_data()
-        
-        # Write DataFrame to a csv file.
-        df.to_csv('titanic_df.csv')
-        
-    return df
-
-###################### Acquire Iris Data ######################
-
-def new_iris_data():
-    '''
-    This function reads the iris data from the Codeup db into a df.
-    '''
-    sql_query = """
-                SELECT *
-                FROM measurements
-                JOIN species USING(species_id)
-                """
-    
-    # Read in DataFrame from Codeup db.
-    df = pd.read_sql(sql_query, get_db_url('iris_db'))
-    
-    return df
-
-
-def get_iris_data():
-    '''
-    This function reads in iris data from Codeup database, writes data to
-    a csv file if a local file does not exist, and returns a df.
-    '''
-    if os.path.isfile('iris_df.csv'):
-        
-        # If csv file exists read in data from csv file.
-        df = pd.read_csv('iris_df.csv', index_col=0)
-        
-    else:
-        
-        # Read fresh data from db into a DataFrame
-        df = new_iris_data()
-        
-        # Cache data
-        df.to_csv('iris_df.csv')
-        
-    return df
-
-###################### Acquire Iris Data ######################
     
 def new_telco_data():
     '''This function reads the telco data from the Codeup db into a df,
@@ -105,8 +28,6 @@ def new_telco_data():
     
     return df
 
-
-
 def get_telco_data():
     '''
     This function reads in titanic data from Codeup database, writes data to
@@ -115,14 +36,11 @@ def get_telco_data():
     if os.path.isfile('telco_churn.csv'):
         
         # If csv file exists, read in data from csv file.
-        df = pd.read_csv('telco_churn.csv', index_col=0)
-        
-    else:
-        
+        df = pd.read_csv('telco_churn.csv', index_col=0)     
+    else:  
         # Read fresh data from db into a DataFrame.
         df = new_telco_data()
         
         # Write DataFrame to a csv file.
-        df.to_csv('telco_churn.csv')
-        
+        df.to_csv('telco_churn.csv')   
     return df
